7439958ac509f908ce2cad0719cfd90782f151f9
Serg <serhiispodyniuk23@gmail.com>
2025-02-20 12:36:51 +0000

    done homework 7

--- app.js
+++ app.js
@@ -1,87 +1,96 @@
-"use strict";
-//1
-console.log(addThemAll(2,4)); // 6
-console.log(addThemAll(1,2,3,4)); // 10
-console.log(addThemAll(5,5,10)); // 20
-
-function addThemAll(...args) {
-    let sum = 0;
-    for (let num of args) {
-        sum += num;
-    }
-    return sum;
+function detonatorTimer(delay) {
+  let counter = delay;
+  const timer = setInterval(() => {
+      if (counter > 0) {
+          console.log(counter);
+          counter--;
+      } else {
+          console.log('BOOM!');
+          clearInterval(timer);
+      }
+  }, 1000);
 }
 
-//2
-console.log(multiply(5)(5))		// 25
-console.log(multiply(2)(-2))	        // -4
-console.log(multiply(4)(3))		// 12
+detonatorTimer(3);
 
-function multiply(a) {
-    return function(b) {
-      return a * b;
-    };
-  }
-//3
-
-
-const movies = [
-    {
-      movieName: 'The Thing',
-      releaseYear: 1982,
-      directedBy: 'Carpenter',
-      runningTimeInMinutes: 109,
-    },
-    {
-      movieName: 'Aliens',
-      releaseYear: 1986,
-      directedBy: 'Cameron',
-      runningTimeInMinutes: 137,
-    },
-    {
-      movieName: 'Men in Black',
-      releaseYear: 1997,
-      directedBy: 'Sonnenfeld',
-      runningTimeInMinutes: 98,
-    },
-    {
-      movieName: 'Predator',
-      releaseYear: 1987,
-      directedBy: 'McTiernan',
-      runningTimeInMinutes: 107,
-    },
-  ];
-  const sortedByYear = [...movies].sort(byProperty('releaseYear', '>'));
-  console.log(sortedByYear); 
-  // Сортування по року випуску
-  
-  const sortedByTime = [...movies].sort(byProperty('runningTimeInMinutes', '<'));
-  console.log(sortedByTime); 
-  // Сортування по тривалості фільмів
-  
-  const sortedByName = [...movies].sort(byProperty('movieName', '>'));
-  console.log(sortedByName); 
-  // Сортування по назві фільму
-  
-  function byProperty(property, direction) {
-    return function(a, b) {
-      if (typeof a[property] === 'number' && typeof b[property] === 'number') {
-        return direction === '>' ? a[property] - b[property] : b[property] - a[property];
+function detonatorTimer(delay) {
+  function countdown(counter) {
+      if (counter > 0) {
+          console.log(counter);
+          setTimeout(() => countdown(counter - 1), 1000);
+      } else {
+          console.log('BOOM!');
       }
-      if (typeof a[property] === 'string' && typeof b[property] === 'string') {
-        return direction === '>' 
-          ? a[property].localeCompare(b[property]) 
-          : b[property].localeCompare(a[property]);
-      }
-  
-      return 0;
-    };
   }
-//4
-const userNames = ['Петро', 'Емма', 'Петро', 'Емма', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена', 'Емма'];
-
-function filterUnique(array) {
-  return [...new Set(array)];
+  countdown(delay);
 }
 
-console.log(filterUnique(userNames)); // ['Петро', 'Емма', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена']
+detonatorTimer(3);
+
+
+
+let car = {
+  brand: 'Tesla',
+  model: 'Model S',
+  year: 2022,
+  color: 'red',
+  maxSpeed: 250,
+  describeCar() {
+      console.log(`This is a ${this.color} ${this.brand} ${this.model} from ${this.year}.`);
+  },
+  accelerate() {
+      console.log(`The ${this.brand} ${this.model} accelerates to its max speed of ${this.maxSpeed} km/h!`);
+  },
+  repaint(newColor) {
+      this.color = newColor;
+      console.log(`The car has been repainted to ${this.color}.`);
+  }
+};
+
+car.describeCar();
+car.accelerate();
+car.repaint('black');
+
+
+
+let car = {
+  brand: 'Tesla',
+  model: 'Model S',
+  year: 2022,
+  color: 'red',
+  maxSpeed: 250,
+  describeCar() {
+      console.log(`This is a ${this.color} ${this.brand} ${this.model} from ${this.year}.`);
+  },
+  accelerate() {
+      console.log(`The ${this.brand} ${this.model} accelerates to its max speed of ${this.maxSpeed} km/h!`);
+  },
+  repaint(newColor) {
+      this.color = newColor;
+      console.log(`The car has been repainted to ${this.color}.`);
+  }
+};
+
+let securedDescribeCar = car.describeCar.bind(car);
+let securedAccelerate = car.accelerate.bind(car);
+let securedRepaint = car.repaint.bind(car, 'black');
+
+setTimeout(securedDescribeCar, 4000);
+setTimeout(securedAccelerate, 5000);
+setTimeout(securedRepaint, 6000);
+
+
+
+function someFunction(a, b) {
+  console.log(`Result: ${a + b}`);
+}
+
+function slower(func, seconds) {
+  return function(...args) {
+      console.log(`Chill out, you will get your result in ${seconds} seconds`);
+      setTimeout(() => func(...args), seconds * 1000);
+  };
+}
+
+let slowedSomeFunction = slower(someFunction, 5);
+slowedSomeFunction(3, 7);
